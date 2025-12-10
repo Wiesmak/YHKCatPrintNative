4275fc11872181b3555808c541cef7d97b8f8b3b
Gerard Gajda <gerard.gajda@gmail.com>
2025-12-10 20:02:46 +0100

    Implement basic text printer in native code

--- nativetest.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include "nativetest.h"
-
-JNIEXPORT void JNICALL Java_pl_umamusume_yhkcatprint_utils_NativeTest_printHelloFromNative(JNIEnv* env, jobject obj) {
-    std::cout << "Hello from Native C++!" << std::endl;
-}
